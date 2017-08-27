90641093619e967ab21f7f1952957d374fa08e86
ne_Sachirou <utakata.c4se@gmail.com>
2017-08-28 00:33:42 +0900

    `mix new mnemonics --sup`

---
+++ lib/mnemonics.ex
@@ -0,0 +1,18 @@
+defmodule Mnemonics do
+  @moduledoc """
+  Documentation for Mnemonics.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mnemonics.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mnemonics_test.exs
@@ -0,0 +1,8 @@
+defmodule MnemonicsTest do
+  use ExUnit.Case
+  doctest Mnemonics
+
+  test "greets the world" do
+    assert Mnemonics.hello() == :world
+  end
+end
