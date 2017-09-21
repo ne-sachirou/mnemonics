6dd646d6a2f7335674c23211c8b138a9d0e46e4a
ne_Sachirou <utakata.c4se@gmail.com>
2017-09-21 20:11:27 +0900

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@ defmodule Mnemonics.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env),
       package: package(),
       start_permanent: Mix.env == :prod,
-      version: "0.1.0",
+      version: "0.1.1",
 
       # Docs
       docs: [
