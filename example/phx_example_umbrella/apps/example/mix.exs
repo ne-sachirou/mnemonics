4b4aab58aa41f8e621724093b5769b48783bac28
ne_Sachirou <utakata.c4se@gmail.com>
2018-04-11 10:46:10 +0900

    WIP

--- example/phx_example_umbrella/apps/example/mix.exs
+++ example/phx_example_umbrella/apps/example/mix.exs
@@ -37,7 +37,8 @@ defmodule Example.Mixfile do
   defp deps do
     [
       {:postgrex, ">= 0.0.0"},
-      {:ecto, "~> 2.1"}
+      {:ecto, "~> 2.1"},
+      {:mnemonics, path: "../../../.."},
     ]
   end
 
